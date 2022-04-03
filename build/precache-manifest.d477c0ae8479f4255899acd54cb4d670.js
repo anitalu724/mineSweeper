16f515152c1774d464d9605bf0e60e4895c34527
Anita Lu <r10921058@ntu.edu.tw>
2022-04-03 14:40:04 +0800

    deploy

---
+++ build/precache-manifest.d477c0ae8479f4255899acd54cb4d670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4905495fb8c8a1fec0",
+    "url": "/mineSweeper/static/css/main.eb76b176.chunk.css"
+  },
+  {
+    "revision": "3c4905495fb8c8a1fec0",
+    "url": "/mineSweeper/static/js/main.3c490549.chunk.js"
+  },
+  {
+    "revision": "81d6e903d8742797526d",
+    "url": "/mineSweeper/static/js/1.81d6e903.chunk.js"
+  },
+  {
+    "revision": "9001d0fdc07c33d5e077",
+    "url": "/mineSweeper/static/js/runtime~main.9001d0fd.js"
+  },
+  {
+    "revision": "21750ed28c0c187e4938f0e76ce5a871",
+    "url": "/mineSweeper/index.html"
+  }
+];
