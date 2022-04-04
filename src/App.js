d18bf23d8013eab19307566795addf99011c236b
Anita Lu <r10921058@ntu.edu.tw>
2022-04-04 12:01:56 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MineSweeper from './containers/MineSweeper';
 
 const App = () => {
   return (
-    <div className = 'App'>
-      <MineSweeper/>
-    </div>
+    <Router>
+      <div className = 'App'>
+      <Switch>
+        <Route path='/'>
+          <MineSweeper/>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
